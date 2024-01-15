4cdd0b4906e8e76b9fa6bd1b60d861223d05f529
dsfedor <154928293+dsfedor@users.noreply.github.com>
2024-01-15 16:49:48 +0300

    Add files via upload

---
+++ PostgreSQL/procfile_pstgr.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/perl
+#!perl -w
+use strict;
+use DBI;
+#open LOG, ">d:/Proclog/log.txt";  #log for debug
+
+#open the file being processed
+open (PRLOG, "c:/log/out") or die "was error $!";
+my @prLog;  #all srings from the log
+my @strPrLog;   #one string from the log
+#get all strings from log
+@prLog = <PRLOG>;
+
+my $hostname = "localhost";
+my $port = "5432";
+my $user = "postgres";
+my $password = "12345";
+my $dbname = "procfile";
+my $dbh = DBI->connect("dbi:Pg:dbname=$dbname;host=$hostname;port=$port",$user,$password);
+
+#main cycle - iterate through all log entries
+foreach (@prLog) {
+
+        @strPrLog = split ;  #get all string values
+        #check flag and choose the table
+        if (/<=/){
+         &procStrIn;
+        }
+        else {
+         &procStrOth;
+        }
+}
+
+#close LOG;
+close PRLOG;
+$dbh->disconnect();
+
+print "finish \n";
+#print LOG "finish\n";
+
+#processing incoming strings
+sub procStrIn {
+
+my $created;
+my $id;
+my $int_id;
+my $str;
+
+     $created = shift(@strPrLog); #add data to created
+     $created = $created . " " . shift(@strPrLog);  #add time to created
+     $str = join " ", @strPrLog; #result without first two fields
+     $int_id = shift(@strPrLog); #add int_id to int_id field
+
+            #find id field
+            foreach (@strPrLog) {
+                     if (/id=/){
+                      $id = substr($_, 3); #cut the first 3 symbols
+                      last;
+                     }
+            }
+
+    if ($id){
+      #if "id" isn't empty - wright the ready fields into database
+      $dbh->do("insert into message (created, id, int_id, str) values('$created','$id','$int_id','$str')")
+    }
+
+ print "into message $created $id $int_id \n";
+
+ }
+
+#processing others strings
+ sub procStrOth {
+
+my $created;
+my $int_id;
+my $str;
+my $address;
+
+     $created = shift(@strPrLog);
+     $created = $created . " " . shift(@strPrLog);
+     $str = join " ", @strPrLog; #result without first two fields
+     $str =~ s/[\\\?\']//g;   #delet "?" and "\" and "'" from string
+     $int_id = shift(@strPrLog);
+
+            #check the first availability the mail address
+            foreach (@strPrLog) {
+                   if (/.+@.+\..+/i){
+                    $address = $_;
+                    last;  #first availability(!)
+                   }
+            }
+
+   #wright the ready fields into database
+   $dbh->do("insert into log (created, int_id, str, address) values('$created','$int_id','$str','$address')");
+
+ print "into log $created $int_id $address \n";
+
+ }
